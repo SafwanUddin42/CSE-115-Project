92cecada7345b525a19ad34f73c2684bfd5a68d8
SafwanUddin42 <137511125+SafwanUddin42@users.noreply.github.com>
2023-06-23 19:48:38 +0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include "SplashScreen.h"
+//#include "login.h"
+//#include "menu.h"
+
+int file_num;
+
+int main(void)
+{
+
+    
+
+    //sscreen();
+
+    //login();
+
+    //main_menu();
+
+
+    return 0;
+
+}
